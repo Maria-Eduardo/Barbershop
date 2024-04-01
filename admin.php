02a25a3c9974487c5902758df27c96ce15c867ce
Maria Eduardo Nelembe <127058062+Maria-Eduardo@users.noreply.github.com>
2024-04-01 16:31:18 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -17,7 +17,7 @@
             <nav class="navbar navbar-expand-lg bg-body-primary" style="display: flex; flex: 1;"  >
                 <div class="container-fluid ">
                 <div class="logo">
-                    <a class="navbar-brand" href="#"><img  src="./images/logo_rb.png" width="110px" alt="logo_img"></a>
+                    <a class="navbar-brand" href="index.php#home"><img  src="./images/logo_rb.png" width="110px" alt="logo_img"></a>
                 </div>    
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
